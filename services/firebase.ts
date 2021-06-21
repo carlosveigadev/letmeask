11d44963756450dc69046e6bcafeb593959fc437
wrack <carlosveiga1618@gmail.com>
2021-06-20 21:20:50 -0300

    add firebase and its config

---
+++ services/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvFPznTJDLIGWwfMQ-Ru_go8skEmlILNY",
+  authDomain: "letmeask-e9cf5.firebaseapp.com",
+  databaseURL: "https://letmeask-e9cf5-default-rtdb.firebaseio.com",
+  projectId: "letmeask-e9cf5",
+  storageBucket: "letmeask-e9cf5.appspot.com",
+  messagingSenderId: "154807155648",
+  appId: "1:154807155648:web:771568ac7dd97256346625",
+  measurementId: "G-7SL8FGM1H7"
+};
+
+firebase.initializeApp(firebaseConfig);

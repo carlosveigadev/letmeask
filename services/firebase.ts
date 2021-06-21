12403e4e41ca71f63b9b0672ee20457b2ca76db3
wrack <carlosveiga1618@gmail.com>
2021-06-20 21:36:27 -0300

    update environment keys

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,14 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
  
 const firebaseConfig = {
-  apiKey: "AIzaSyCvFPznTJDLIGWwfMQ-Ru_go8skEmlILNY",
-  authDomain: "letmeask-e9cf5.firebaseapp.com",
-  databaseURL: "https://letmeask-e9cf5-default-rtdb.firebaseio.com",
-  projectId: "letmeask-e9cf5",
-  storageBucket: "letmeask-e9cf5.appspot.com",
-  messagingSenderId: "154807155648",
-  appId: "1:154807155648:web:771568ac7dd97256346625",
-  measurementId: "G-7SL8FGM1H7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);

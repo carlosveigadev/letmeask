aa51efbbd1954d7cbdacaeb5c2c6cb3f6fdbb784
wrack <carlosveiga1618@gmail.com>
2021-06-22 12:37:59 -0300

    add routes and navigation from home to newRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg';
+import { useHistory } from 'react-router';
 import { Button } from '../components/Button';
 import '../assets/styles/auth.scss';
 
-export const Home = () => (
+export const Home = () => {
+  const history = useHistory();
+
+  const navigateToNewRoom = () => {
+    history.push('/rooms/new');
+  };
+
+  return (
   <div id="page-auth">
     <aside>
       <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
@@ -14,7 +22,7 @@ export const Home = () => (
     <main>
       <div className="main-content">
         <img src={logoImg} alt="Letmeask logo" />
-        <button type="submit" className="create-room">
+        <button type="submit" onClick={navigateToNewRoom} className="create-room">
           <img src={googleImg} alt="Google logo" />
           Create your room with Google
         </button>
@@ -31,4 +39,4 @@ export const Home = () => (
       </div>
     </main>
   </div>
-);
+)};

950628dc294811ef0850438cbd2e8f03b859567d
wrack <carlosveiga1618@gmail.com>
2021-06-23 00:27:56 -0300

    add backbone of rooms page

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,5 +1,34 @@
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button'
+
 export const Room = (): JSX.Element => {
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask logo" />
+          <div>room code</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Room React</h1>
+          <span>4 questions</span>
+        </div>
+
+        <form>
+          <textarea
+            placeholder="What is your question?"
+          />
+
+          <div className="form-footer">
+            <span>Please <button>login</button> to send a question.</span>
+            <Button type="submit">Send Question</Button>
+          </div>
+        </form>
+
+      </main>
+    </div>
   )
 }

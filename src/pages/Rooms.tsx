098257b8a094e15394a8c0fa2eb8ac8e35f25d53
wrack <carlosveiga1618@gmail.com>
2021-06-23 13:29:08 -0300

    pass params to from router to RoomCode

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,15 +1,22 @@
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useParams } from 'react-router';
 import '../assets/styles/rooms.scss';
 
+type RoomParams = {
+  id: string;
+}
+
 export const Room = (): JSX.Element => {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask logo" />
-          <RoomCode code="123123123" /> 
+          <RoomCode code={params.id} /> 
         </div>
       </header>
 

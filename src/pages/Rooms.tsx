e7ce7149e2a60a79cd18020f47b1075aa7826e2d
wrack <carlosveiga1618@gmail.com>
2021-06-23 13:36:57 -0300

    add logic to send new question to firebase

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -3,20 +3,51 @@ import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { useParams } from 'react-router';
 import '../assets/styles/rooms.scss';
+import { FormEvent, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
 }
 
 export const Room = (): JSX.Element => {
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const [newQuestion, setNewQuestion] = useState('');
+
+  const handleCreateSendQuestion = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      alert('You need to be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user?.name,
+        avatar: user?.avatar,
+      },
+      isHighlighted: false, 
+      isAnswered: false,
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+  }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask logo" />
-          <RoomCode code={params.id} /> 
+          <RoomCode code={roomId} /> 
         </div>
       </header>
 
@@ -26,9 +57,11 @@ export const Room = (): JSX.Element => {
           <span>4 questions</span>
         </div>
 
-        <form>
+        <form onSubmit={handleCreateSendQuestion}>
           <textarea
             placeholder="What is your question?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
 
           <div className="form-footer">

696ca3dc284063f7a405eb8e432c563399b97e77
wrack <carlosveiga1618@gmail.com>
2021-06-22 23:56:40 -0300

    add function to handle when user wants to join a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,17 +5,38 @@ import { useHistory } from 'react-router';
 import { Button } from '../components/Button';
 import '../assets/styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export const Home = (): JSX.Element => {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
-
+  const [roomCode, setRoomCode] = useState('');
+  
   const handleCreateRoom = async () => {
     if (!user) {
       await signInWithGoogle();
     }
 
     history.push('/rooms/new');
+  }
+
+  const handleJoinRoom = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -33,10 +54,12 @@ export const Home = (): JSX.Element => {
           Create your room with Google
         </button>
         <div className="separator">or join a room</div>
-        <form>
+        <form onSubmit={handleJoinRoom}>
           <input
             type="text"
             placeholder="Type the room code"
+            onChange={event => setRoomCode(event.target.value)}
+            value={roomCode}
           />
           <Button type="submit">
             Join the room

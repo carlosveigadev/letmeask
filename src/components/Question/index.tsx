66e46747fd4ba8d806b3ac826d368471bfe03da2
wrack <carlosveiga1618@gmail.com>
2021-06-24 15:05:41 -0300

    add like button and style it

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import './styles.scss';
 
 type QuestionProps = {
@@ -5,18 +6,24 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
+  };
+  children?: ReactNode;
 }
 
 export const Question = ({
   content, 
-  author
+  author,
+  children
 }: QuestionProps): JSX.Element => (
   <div className="question">
     <p>{content}</p>
     <footer>
       <div className="user-info">
         <img src={author.avatar} alt={author.name} />
+        <span>{author.name}</span>
+      </div>
+      <div>
+        {children}
       </div>
     </footer>
   </div>

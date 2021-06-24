3b154a568532cbcbfbe109ec516ff5c044b1617b
wrack <carlosveiga1618@gmail.com>
2021-06-24 15:42:32 -0300

    finish logic to verify if room was already finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,6 +36,11 @@ export const Home = (): JSX.Element => {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Room already closed');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
